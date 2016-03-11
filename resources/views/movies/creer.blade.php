9161929f5b498510bab5a5928bb422547d0ce0bc
root <aurelien.oudin@free.fr>
2016-03-11 17:15:21 +0100

    message

--- resources/views/movies/creer.blade.php
+++ resources/views/movies/creer.blade.php
@@ -14,30 +14,38 @@
             <div class = "form-group">
                 <label for = "title">Titre</label>
                 <input name ="title" id="title" >
+            </div>
 
-
+            <div class = "form-group">
                 <label for ="synopsis">Synopsis</label>
                 <input name= "synopsis" id="synopsis">
             </div>
+            <div class = "form-group">
                  <label for="budget">Budget</label>
                  <input name="budget" id="budget">
 
-            <label for="annee">Année</label>
-            <input name="annee" id="annee">
-            <div>
-            <label for="cover">Couverture</label>
-            <input name="cover" id = "cover">
+            <div class = "form-group">
+                 <label for="annee">Année</label>
+                 <input name="annee" id="annee">
+            </div>
 
-            <label for="language">Langue : </label>
-            <input type="radio" name="language"  id = "language" value="fr"> fr
-            <input type="radio" name="language" id = "language" value="en"> en
-            <input type="radio" name="language" id = "language" value="es"> es
+            <div class = "form-group">
+                 <label for="cover">Couverture</label>
+                 <input name="cover" id = "cover">
+            </div>
+
+            <div class = "form-group">
+                <label for="language">Langue : </label>
+                 <input type="radio" name="language"  id = "language" value="fr"> fr
+                 <input type="radio" name="language" id = "language" value="en"> en
+                 <input type="radio" name="language" id = "language" value="es"> es
             </div>
 
             <label for = "description">Description</label>
             <textarea id="description" name="description"></textarea>
 
-            <select name="BO">
+            <select name ="BO">
+            <label for="BO">BO</label>
             <option>Select BO</option>
             <option>Vo</option>
             <option>Vost</option>
--- resources/views/movies/list.blade.php
+++ resources/views/movies/list.blade.php
@@ -1,52 +1,9 @@
-
-<html>
-    <head>
-        <title>Film</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 10px;
-                padding: 10px;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            h3 {
-                font-size: 50px;
-                color: Black;
-                text-align: center;
-
-            }
-
-            img {
-                width: 20%;
+@extends('layout')
 
 
-            }
+@section('content')
 
-        </style>
-    </head>
 
-    <body>
     <div class = "container">
         <div class = "content">
 
@@ -78,5 +35,4 @@
         </div>
 
     </div>
-    </body>
-</html>
+
